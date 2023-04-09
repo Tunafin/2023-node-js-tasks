e2efaf51fd90cbc76bcb4e6f8139ab221030c3ed
tunafin <tunaweichi@gmail.com>
2023-04-09 17:29:42 +0800

    Adjust task-03 code

--- task-03/models/posts.model.js
+++ task-03/models/posts.model.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const postsSchema = new mongoose.Schema({
+
+const definition = {
   name: {
     type: String,
     required: [true, '貼文姓名未填寫']
@@ -36,8 +37,18 @@ const postsSchema = new mongoose.Schema({
     type: Number,
     default: 0
   },
-});
+};
 
+const options = { versionKey: false /** ,collection: 'posts' */ };
+
+const postsSchema = new mongoose.Schema(definition, options);
+
+/** 
+ * mongoose.model的第一個參數會被自動轉換，需特別注意。
+ * 例: MyCollection => mycollections
+ * 
+ * 若不想被轉換，可在new mongoose.Schema(definition, options)的options中設定collection名稱
+ */
 const posts = mongoose.model(
   'posts',
   postsSchema
